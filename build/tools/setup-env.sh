26679cac252154489ea4b3db73ab850b60365ef1
kiootic <kiootic@oursky.com>
2022-05-23 11:27:23 +0800

    Fix runner path

--- build/tools/setup-env.sh
+++ build/tools/setup-env.sh
@@ -3,3 +3,4 @@ set -euxo pipefail
 
 echo 'HELM_PLUGINS=/ci/tools/helm/plugins' | tee -a /etc/environment
 echo "ANDROID_HOME=/ci/tools/android" | tee -a /etc/environment
+sed 's/:\/usr\/bin:/:\/usr\/bin:\/ci\/bin:/' -i /etc/environment
