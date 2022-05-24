a04bf2c665aa3b13acf739c2e3acc8026bad275b
kiootic <kiootic@oursky.com>
2022-05-25 02:13:28 +0800

    Install GKE auth plugin

--- build/tools/04-gcloud.sh
+++ build/tools/04-gcloud.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 set -euxo pipefail
 
+sudo apt-get install -y python-is-python3
+
 mkdir -p /ci/tools
 curl -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-386.0.0-linux-x86_64.tar.gz | \
     tar xzC /ci/tools
+/ci/tools/google-cloud-sdk/bin/gcloud components install gke-gcloud-auth-plugin
 
 pushd /ci/tools/google-cloud-sdk/bin
 for bin in *; do
