75a4b66e4f0e85f86245bafdb9e814506683e36a
kiootic <kiootic@oursky.com>
2022-06-06 16:55:41 +0800

    Fix flutter not detecting SDK manager

--- build/tools/03-android.sh
+++ build/tools/03-android.sh
@@ -9,10 +9,10 @@ curl -L https://dl.google.com/android/repository/commandlinetools-linux-8512546_
 unzip -d "$ANDROID_HOME/cmdline-tools" /tmp/commandlinetools.zip
 rm /tmp/commandlinetools.zip
 
-mv "$ANDROID_HOME/cmdline-tools/cmdline-tools" "$ANDROID_HOME/cmdline-tools/tools"
-pushd "$ANDROID_HOME/cmdline-tools/tools/bin"
+mv "$ANDROID_HOME/cmdline-tools/cmdline-tools" "$ANDROID_HOME/cmdline-tools/latest"
+pushd "$ANDROID_HOME/cmdline-tools/latest/bin"
 for bin in *; do
     test -f "$bin" && test -x "$bin" && \
-        ln -s "$ANDROID_HOME/cmdline-tools/tools/bin/$bin" "/ci/bin/$bin"
+        ln -s "$ANDROID_HOME/cmdline-tools/latest/bin/$bin" "/ci/bin/$bin"
 done
 popd
