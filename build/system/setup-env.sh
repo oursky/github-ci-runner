f7c8ee6c1fb9e206b87fec26cfe67bdc74633c15
kiootic <kiootic@oursky.com>
2022-06-06 17:27:35 +0800

    Fix environment variable interpolation

--- build/system/setup-env.sh
+++ build/system/setup-env.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 echo 'DEBIAN_FRONTEND=noninteractive' | tee -a /etc/environment
 echo 'ImageOS=ubuntu22' | tee -a /etc/environment
-echo 'XDG_CONFIG_HOME=$HOME/.config' | tee -a /etc/environment
+echo 'XDG_CONFIG_HOME=/home/runner/.config' | tee -a /etc/environment
