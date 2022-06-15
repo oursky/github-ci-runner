3fa74a8b5aba814c8e0c89cfb80398328ae8221c
kiootic <kiootic@oursky.com>
2022-06-15 14:56:35 +0800

    Adjust runner hostname

--- macOS/volumes/provisioning/system/payload/private/var/provisioner/daemon-scripts/01-hostname.sh
+++ macOS/volumes/provisioning/system/payload/private/var/provisioner/daemon-scripts/01-hostname.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # https://github.com/actions/virtual-environments/blob/macOS-12/20220605.1/images/macos/provision/configuration/configure-hostname.sh
 
-name="runner-$(openssl rand -hex 3)"
+name="mac-runner-$(openssl rand -hex 3)"
 scutil --set HostName "${name}.local"
 scutil --set LocalHostName $name
 scutil --set ComputerName "${name}.local"
