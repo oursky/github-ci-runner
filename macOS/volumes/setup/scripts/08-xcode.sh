c1223871e33ab4b2106f42611df9389c7b707f39
kiootic <kiootic@oursky.com>
2022-06-17 21:47:47 +0800

    Fix Xcode setup

--- macOS/volumes/setup/scripts/08-xcode.sh
+++ macOS/volumes/setup/scripts/08-xcode.sh
@@ -2,11 +2,11 @@
 set -euxo pipefail
 
 for APP in /Volumes/xcodes/*.app; do
-    sudo xcode-select -s "$APP"
-    sudo xcodebuild -license accept
-    sudo xcodebuild -runFirstLaunch
+    sudo DEVELOPER_DIR="$APP" xcrun xcodebuild -license accept
+    sudo DEVELOPER_DIR="$APP" xcrun xcodebuild -runFirstLaunch
+    # Trigger app verification
+    DEVELOPER_DIR="$APP" xcrun simctl list
 done
-sudo xcode-select -r
 
 CERTIFICATES=(
     'AppleWWDRCAG3.cer'
--- macOS/volumes/xcodes/optimize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-set -euxo pipefail
-
-jdupes -r -B /Volumes/xcodes -q 2>/dev/null
