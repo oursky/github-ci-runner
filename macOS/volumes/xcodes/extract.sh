63351dd19103dfa458621dbdad652a0a0ed5114a
Raymond <me@keis.in>
2022-06-19 22:57:32 +0800

    Fix xcode image build

--- macOS/volumes/xcodes/extract.sh
+++ macOS/volumes/xcodes/extract.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 mkdir -p /Volumes/xcodes/staging
 pushd xips
 for XIP in *.xip; do
-    test -e "../apps/${XIP%\+*}.app" && continue
+    test -e "/Volumes/xcodes/${XIP%\+*}.app" && continue
     ../../../bin/unxip "$XIP" "/Volumes/xcodes/staging"
     mv "/Volumes/xcodes/staging/Xcode.app" "/Volumes/xcodes/${XIP%\+*}.app"
 done
