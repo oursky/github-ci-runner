e0f07696960bde65fa6dcbbe5ccfbcde4bf70e4c
kiootic <kiootic@oursky.com>
2022-06-19 23:35:51 +0800

    Install CLT though Homebrew installer

--- macOS/volumes/setup/scripts/01-rosetta.sh
+++ macOS/volumes/setup/scripts/01-rosetta.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-sudo installer -package ../assets/CommandLineTools_*.pkg -target /
-
 sudo softwareupdate --install-rosetta --agree-to-license
 
 sudo mkdir -p /usr/local/bin
