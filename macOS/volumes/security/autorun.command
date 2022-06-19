26c3748eb42337cb9bb5fa6c7af19b9b981278b2
kiootic <kiootic@oursky.com>
2022-06-19 23:27:43 +0800

    Merge certificate import into setup script

--- macOS/volumes/security/autorun.command
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-set -euxo pipefail
-
-pushd scripts
-for script in *.sh; do
-    bash -l "./$script"
-done
--- macOS/volumes/setup/scripts/08-xcode.sh
+++ macOS/volumes/setup/scripts/08-xcode.sh
@@ -7,3 +7,14 @@ for APP in /Volumes/xcodes/*.app; do
     # Trigger app verification
     DEVELOPER_DIR="$APP" xcrun simctl list
 done
+
+CERTIFICATES=(
+    'AppleWWDRCAG3.cer'
+    'DeveloperIDG2CA.cer'
+)
+sudo security authorizationdb write com.apple.trust-settings.admin allow
+for CERT in "${CERTIFICATES[@]}"; do
+    curl -o "/tmp/$CERT" "https://www.apple.com/certificateauthority/$CERT"
+    security add-trusted-cert -d -r unspecified -k login.keychain "/tmp/$CERT"
+    rm "/tmp/$CERT"
+done
