30a93516edc0e4411d32f5e5544eea4731745d4b
kiootic <kiootic@oursky.com>
2025-01-15 11:12:09 +0800

    Pin poetry version

--- linux/build/system/packages/06-python.sh
+++ linux/build/system/packages/06-python.sh
@@ -5,7 +5,7 @@ apt-fast install --no-install-recommends python3 python3-dev python3-pip python3
 
 export PATH="$PATH:/home/runner/.local/bin"
 sudo -u runner python3 -m pip install pipx
-sudo -u runner pipx install poetry
+sudo -u runner pipx install poetry==2.0.1
 
 cat <<EOF >> /etc/profile.d/10-python.sh
 export PATH="\$PATH:/home/runner/.local/bin"
